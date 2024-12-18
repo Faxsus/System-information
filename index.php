366c9cf0d23ee675b98e4bb989b0018dd65bf53f
Faxs <adamfajar82@gmail.com>
2024-12-18 09:36:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Simple Website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <div class="main">
+            <div class="logo">
+                <img src="logo.webp" alt="Logo">
+            </div>
+            <ul>
+                <li class="active"><a href="#"><i class="fa fa-home"></i> Home</a></li>
+                <li><a href="animation.php">About</a></li> <!-- Perubahan di sini -->
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Location</a></li>
+                <li><a href="#">Achievement</a></li>
+                <li>
+                    <a href="#"><i class="fa fa-caret-down"></i> Download <i class="fa fa-caret-down"></i></a>
+                    <div class="sub-menu">
+                        <ul>
+                            <li><a href="#">Certificate</a></li>
+                            <li><a href="#">Accreditation</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="title">
+            <h1>Statistics Enthusiastic</h1>
+        </div>
+        <div class="button">
+            <a href="#" class="btn">Skill</a>
+            <a href="#" class="btn">Video</a>
+            <a href="#" class="btn">More</a>
+        </div>
+    </header>
+</body>
+</html>
